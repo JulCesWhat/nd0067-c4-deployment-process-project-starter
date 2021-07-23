dc7e81ed973701c7201e1e1946d520143f5aebac
JulCesWhat <julceswhat@gmail.com>
2021-07-22 20:23:03 -0400

    Adding env variables

--- udagram-api/src/aws.ts
+++ udagram-api/src/aws.ts
@@ -9,8 +9,8 @@ const credentials = new AWS.SharedIniFileCredentials({ profile: "default" });
 AWS.config.credentials = credentials;
 
 AWS.config.update({
-  accessKeyId: process.env.AWS_ACCESSKEYID,
-  secretAccessKey: process.env.AWS_SECRETACCESSKEY
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 });
 
 export const s3 = new AWS.S3({
